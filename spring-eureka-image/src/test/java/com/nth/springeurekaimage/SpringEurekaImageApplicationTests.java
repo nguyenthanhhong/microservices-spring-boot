aad957320fcf52eef97b17f3c6268bebb5d03cac
Hong Nguyen <nguyenthanhhong26@gmail.com>
2020-06-24 10:30:46 +0700

    init

---
+++ spring-eureka-image/src/test/java/com/nth/springeurekaimage/SpringEurekaImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nth.springeurekaimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
