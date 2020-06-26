d36a7621bed9ab2a068364ab2e8d5b649a59c1c7
Hong Nguyen <nguyenthanhhong26@gmail.com>
2020-06-26 09:31:53 +0700

    init

---
+++ spring-eureka-common/src/main/java/com/nth/springeurekacommon/SpringEurekaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.nth.springeurekacommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaCommonApplication.class, args);
+	}
+
+}
---
+++ spring-eureka-common/src/test/java/com/nth/springeurekacommon/SpringEurekaCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nth.springeurekacommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
