fbd51200a4f072df698804ad194f469f3f474e80
Hong Nguyen <nguyenthanhhong26@gmail.com>
2020-06-26 09:15:34 +0700

    init

---
+++ spring-eureka-auth/src/test/java/com/nth/springeurekaauth/SpringEurekaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nth.springeurekaauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
